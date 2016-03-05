c94759fe6612f2d77dd283445ceaf18d1202aacc
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-03-05 00:28:47 -0800

    Add setTimeout - jasmine works!

--- spec/helloworld.js
+++ spec/helloworld.js
@@ -5,14 +5,18 @@ describe("Hello World Server", function() {
   describe("GET /", function() {
     it("return status 200", function() {
       request.get(base_url, function(error, response, body) {
-        expect(response.statuscode).toBe(200);
+        expect(response.statusCode).toBe(200);
       });
     });
-
     it("returns Hello World", function(done) {
       request.get(base_url, function(error, response, body) {
-        expect(body).toBe("Hello World");
-      })
+        console.log(body);
+        expect(body).toBe("Hello World!");
+        setTimeout(function () {
+            console.log('inside timeout');
+            done();
+        }, 500);
+      });
     });
   });
 });
