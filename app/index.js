76657b3353a585454b04cc343410bc08e20bf20a
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-03-05 01:08:23 -0800

    Add urls to typeform API and Slack API

--- app/index.js
+++ app/index.js
@@ -5,6 +5,20 @@ var $ = require('jQuery');
 
 var app = express();
 
+// Typefrom urls
+  var typeform_api_1 = "https://api.typeform.com/v0/form/"
+var typeform_form_key = "gUl2lY"; // YOUR FORM KEY
+var typeform_key = "?key=c715b7dacbe3548df13672b5066f9d2b1080201b"; // YOUR API KEY
+var typeform_api_2 = "&completed=true&offset=0";
+var typeform_url = typeform_api_1 + typeform_form_key +
+                   typeform_key + typeform_api_2;
+
+// Slack API url
+var slack_teamname = "https://ssushi-clarifaiapi.slack.com" // YOUR TEAM URL
+var slack_api_1 = "/api/users.admin.invite?t="
+var curr_time = (new Date).getTime().toString(); // current time in epoche
+var slack_url = slack_teamname + slack_api_1 + curr_time;
+
 app.get('/', function (req, res) {
     res.status(200).send('Hello World!')
 });
