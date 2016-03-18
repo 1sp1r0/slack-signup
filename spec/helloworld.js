f47bbb68790eb3677df1891d1d7cc43ba6ab1a62
Atsushi Yamamoto <yamaatsushi927@gmail.com>
2016-03-18 12:30:43 -0700

    Delete jasmine 'Return Hello World' test

--- spec/helloworld.js
+++ spec/helloworld.js
@@ -1,21 +1,11 @@
 var request = require("request");
-var base_url = "http://localhost:3000/"
+var base_url = "https://tf-slack-signup.herokuapp.com/"
 
 describe("Hello World Server", function() {
   describe("GET /", function() {
     it("return status 200", function() {
       request.get(base_url, function(error, response, body) {
         expect(response.statusCode).toBe(200);
-      });
-    });
-    it("returns Hello World", function(done) {
-      request.get(base_url, function(error, response, body) {
-        console.log(body);
-        expect(body).toBe("Hello World!");
-        setTimeout(function () {
-            console.log('inside timeout');
-            done();
-        }, 500);
       });
     });
   });
